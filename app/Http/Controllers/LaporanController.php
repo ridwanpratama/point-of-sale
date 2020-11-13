694baf2c72f72a533cf409eea37122122b740136
Muhammad Ridwan Pratama <56720407+ridwanz02@users.noreply.github.com>
2020-11-13 20:26:36 +0700

    Print laporan

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -10,7 +10,7 @@ class LaporanController extends Controller
 {
     public function index()
     {
-        $transaksi = Transaksi::latest()->paginate(5);
+        $transaksi = Transaksi::latest()->get();
  
         return view('laporan.transaksi',compact('transaksi'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
@@ -18,7 +18,7 @@ class LaporanController extends Controller
 
     public function barang()
     {
-        $barang = Barang::latest()->paginate(5);
+        $barang = Barang::latest()->get();
  
         return view('laporan.barang',compact('barang'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
@@ -56,5 +56,57 @@ class LaporanController extends Controller
         $barang = Barang::whereBetween('tanggal_masuk', [$startDate,$endDate])->latest()->paginate(5);
  
         return view('laporan.barang', compact('barang', 'startDate', 'endDate'))->with('i', (request()->input('page', 1) - 1) * 5);;
+    }
+
+    public function printTransaksi(Request $request)
+    {
+ 
+        $transaksi = $request->transaksi;
+        $from = $request->startDate;
+        $to = $request->endDate;
+ 
+        $title ="Laporan From: ".$from."To:".$to;
+        $redirect = route('laporan');   
+        if(isset($from) && isset($to)){
+            $startDate = $from;
+            $endDate = $to;
+ 
+            $transaksi = Transaksi::whereBetween('tanggal_beli', [$startDate,$endDate])->latest()->get();
+            $startDate = explode(' ', $startDate)[0];
+            $endDate = explode(' ', $endDate)[0];
+ 
+            return view('laporan.print-transaksi', compact('transaksi', 'startDate', 'endDate', 'redirect'))->with('i', (request()->input('page', 1) - 1) * 5);
+        }else{
+            $transaksi = Transaksi::latest()->get();
+ 
+            return view('laporan.print-transaksi', compact('transaksi', 'redirect'))->with('i', (request()->input('page', 1) - 1) * 5);
+        }
+ 
+    }
+
+    public function printBarang(Request $request)
+    {
+ 
+        $barang = $request->barang;
+        $from = $request->startDate;
+        $to = $request->endDate;
+ 
+        $title ="Laporan From: ".$from."To:".$to;
+        $redirect = route('laporan');   
+        if(isset($from) && isset($to)){
+            $startDate = $from;
+            $endDate = $to;
+ 
+            $barang = Barang::whereBetween('tanggal_masuk', [$startDate,$endDate])->latest()->get();
+            $startDate = explode(' ', $startDate)[0];
+            $endDate = explode(' ', $endDate)[0];
+ 
+            return view('laporan.print-barang', compact('barang', 'startDate', 'endDate', 'redirect'))->with('i', (request()->input('page', 1) - 1) * 5);
+        }else{
+            $barang = Barang::latest()->get();
+ 
+            return view('laporan.print-barang', compact('barang', 'redirect'))->with('i', (request()->input('page', 1) - 1) * 5);
+        }
+ 
     }
 }
