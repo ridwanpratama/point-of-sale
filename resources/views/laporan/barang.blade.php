397b8805a69d15362f48fda3a4e5d58e311d4c9a
Muhammad Ridwan Pratama <56720407+ridwanz02@users.noreply.github.com>
2020-11-13 14:43:10 +0700

    Form Laporan barang
    Laporan barang berdasarkan tanggal masuk

---
+++ resources/views/laporan/barang.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.master')
+@section('title', 'POS - Laporan')
+@section('pagetitle')
+    <h1>Laporan Barang</h1>
+@endsection
+@section('content')
+<div class="section-body">
+    <div class="row">
+        <div class="col-12 col-md-12 col-lg-12">
+                <form action="/laporan/search" method="GET">
+                @csrf
+                <div class="form-row align-items-center">
+                    <div class="col-auto">
+                        <input type="date" class="form-control @error('startDate') is-invalid @enderror mb-3" name="startDate" id="startDate">
+                        @error('starDate')
+                            <div class="invalid-feedback">{{$message}}</div>
+                            @enderror
+                    </div>
+                    <div class="col-auto">
+                        <label class="col-sm-2 mb-3"><b>-</b></label>
+                    </div>
+                    <div class="col-auto">
+                        <input type="date" class="form-control @error('endDate') is-invalid @enderror mb-3" name="endDate" id="endDate">
+                        @error('endDate')
+                        <div class="invalid-feedback">{{$message}}</div>
+                            @enderror
+                    </div>
+                    <div class="col-auto">
+                        <button type="submit" class="btn btn-primary mb-3">Cari</button>
+                    </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+</form>
+<div class="card">
+    <div class="card-body">
+    <table class="table table-striped table-bordered table-md">
+        <tr>
+            <th>No</th>        
+            <th>Kode Barang</th>
+            <th>Nama Barang</th>
+            <th>Tanggal Masuk</th>
+            <th>Harga Barang</th>
+            <th>Stok Barang</th>
+            <th>Nama Merek</th>
+            <th>Nama Distributor</th>
+        </tr>
+        @foreach ($barang as $item)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $item->kd_barang }}</td>
+            <td>{{ $item->nama_barang }}</td>
+            <td>{{ $item->tanggal_masuk }}</td>
+            <td>{{ $item->harga_barang }}</td>
+            <td>{{ $item->stok_barang }}</td>
+            <td>{{ $item->merek->merek }}</td>
+            <td>{{ $item->distributor->nama_distributor }}</td>
+        </tr>
+        @endforeach
+    </table>
+</div>
+@endsection
