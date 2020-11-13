edf6c9b816fe5588985ae04ce315e2de321f80ab
Muhammad Ridwan Pratama <56720407+ridwanz02@users.noreply.github.com>
2020-11-13 14:42:27 +0700

    Form Laporan transaksi

---
+++ resources/views/laporan/transaksi.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.master')
+@section('title', 'POS - Laporan')
+@section('pagetitle')
+    <h1>Laporan Transaksi</h1>
+@endsection
+@section('content')
+<div class="section-body">
+    <div class="row">
+        <div class="col-12 col-md-12 col-lg-12">
+                <form action="/laporan/cari" method="GET">
+                @csrf
+                <div class="form-row align-items-center">
+                    <div class="col-auto">
+                        <input type="date" class="form-control @error('startDate') is-invalid @enderror mb-3" name="startDate" id="startDate">
+                        @error('starDate')
+                            <div class="invalid-feedback">{{$message}}</div>
+                            @enderror
+                    </div>
+                    <div class="col-auto">
+                        <label class="col-sm-2 mb-3"><b>-</b></label>
+                    </div>
+                    <div class="col-auto">
+                        <input type="date" class="form-control @error('endDate') is-invalid @enderror mb-3" name="endDate" id="endDate">
+                        @error('endDate')
+                        <div class="invalid-feedback">{{$message}}</div>
+                            @enderror
+                    </div>
+                    <div class="col-auto">
+                        <button type="submit" class="btn btn-primary mb-3">Cari</button>
+                    </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+</form>
+<div class="card">
+    <div class="card-body">
+    <table class="table table-striped table-bordered table-md">
+        <tr>
+            <th>No</th>        
+            <th>Kode Transaksi</th>
+            <th>Nama Barang</th>
+            <th>Nama User</th>
+            <th>Jumlah Beli</th>
+            <th>Total Harga</th>
+           	<th>Tanggal Beli</th>
+        </tr>
+        @foreach ($transaksi as $item)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $item->kd_transaksi }}</td>
+            <td>{{ $item->barang->nama_barang }}</td>
+            <td>{{ $item->user_id }}</td>
+            <td>{{ $item->jumlah_beli }}</td>
+            <td>{{ $item->total_harga }}</td>
+            <td>{{ $item->tanggal_beli }}</td>
+        </tr>
+        @endforeach
+    </table>
+</div>
+@endsection
